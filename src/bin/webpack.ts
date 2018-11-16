a592019e5b3010956be9035a57316de0eb191d1f
Gregor Adams <greg@pixelass.com>
2018-11-16 02:27:47 +0100

    fix: path to local webpack files was incorrect

--- src/bin/webpack.ts
+++ src/bin/webpack.ts
@@ -1,18 +1,18 @@
 import path from "path";
 import webpack from "webpack";
 import merge from "webpack-merge";
-import dev from "../../webpack.dev.js";
-import prod from "../../webpack.prod.js";
-import log from "./log";
 import getConfig from "./load";
+import log from "./log";
 
 const {env, argv} = process;
 
 const build = async (watch: boolean) => {
 	const cwd = process.cwd();
 	const {imhotep} = await getConfig();
-	const prodOptions = merge({}, prod(env, argv));
-	const devOptions = merge({}, dev(env, argv));
+	const localDev = require(path.resolve(__dirname, "../../../webpack.dev.js")) || {};
+	const localProd = require(path.resolve(__dirname, "../../../webpack.prod.js")) || {};
+	const prodOptions = merge({}, localProd(env, argv));
+	const devOptions = merge({}, localDev(env, argv));
 	devOptions.entry = path.resolve(cwd, imhotep.entry);
 	devOptions.output.path = imhotep.output.path;
 	prodOptions.entry = path.resolve(cwd, imhotep.entry);
